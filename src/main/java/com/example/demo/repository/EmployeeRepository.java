202e9d7f03c6ddaa467dc05fd32de5107673fd86
idriss <idrissaithadou@gmail.com>
2022-07-01 17:45:45 +0100

    spring boot rest api

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -4,5 +4,5 @@ import com.example.demo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
+    Employee findByFirstName(String firstname);
 }
